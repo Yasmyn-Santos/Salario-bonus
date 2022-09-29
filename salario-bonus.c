c74a9d88abb7be7e647ebde873101d4771f98737
Yasmyn Santos <108375544+Yasmyn-Santos@users.noreply.github.com>
2022-09-29 15:22:06 -0300

    Update salario-bonus.c

--- salario-bonus.c
+++ salario-bonus.c
@@ -10,14 +10,14 @@ salário fixo do vendedor e montante total das vendas efetuadas por este vendedo
 Saída
 Imprima o total que o funcionário deverá receber, conforme exemplo fornecido. */
 
+ 
 int main() {
     
-    char nome;
-    double A,B;
-    scanf("%s",&nome);
-    scanf("%lf %lf",&A,&B);
+    int cd_pc1, num_pc1, cd_pc2, num_pc2;
+    double val1, val2;
+    scanf("%d %d %lf %d %d %lf", &cd_pc1, &num_pc1, &val1, &cd_pc2, &num_pc2, &val2);
     
-    printf("TOTAL = R$ %.2f\n",(A+B*0.15));
+    printf("VALOR A PAGAR: R$ %.2lf\n", (num_pc1*val1)+(num_pc2*val2));
     return 0;
 }
 
